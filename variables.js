f1305765cc191f0ce3201d2aeabbd82f97ea2920
chntlg <chantalguerrero04@gmail.com>
2024-09-09 18:55:44 +0800

    Initial commit with variable.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+name = "Chantal G. Guerrero";
+whatDoYouWannaBecomeInYourLife ="Programmer";
+gender = "Female";
+twitterHandle = "@chntl00416";
+console.log("Name : " , name);
+console.log("What do you want to become in your life : " ,  whatDoYouWannaBecomeInYourLife);
+console.log("Gender : " , gender);
+console.log("Twitter Handle : " , twitterHandle);
